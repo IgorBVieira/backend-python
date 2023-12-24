932c8b81d3b71c46b4b12a9cbc4f0fd3f5074c81
IgorBVieira <igorvieira.org@gmail.com>
2023-12-24 19:31:05 -0300

    alter from recursive lambda to def

--- fibonacci.py
+++ fibonacci.py
@@ -1,7 +1,6 @@
 def execute(x):
-    fibonacci = (lambda x, x_1=1, x_2=0: 
-    x_2 if x == 0
-    else fibonacci(x - 1, x_1 + x_2, x_1))
+    def fibonacci(x, x_1=1, x_2=0):
+        return x_2 if x == 0 else fibonacci(x - 1, x_1 + x_2, x_1)
     return fibonacci(x)
 
 if __name__=='__main__':
